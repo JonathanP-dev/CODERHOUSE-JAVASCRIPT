b02485a8c4cef467074a6783dbdd40b034a02451
JonathanPereira7 <jona714247@gmail.com>
2023-05-17 11:42:49 -0300

    Segunda pre-entrega Jonathan Pereira

--- js/app.js
+++ js/app.js
@@ -1,26 +1,76 @@
-const numbers = []
+// aplicacion para ver informacion de peliculas
+// la aplicacion va a obtener las peliculas y su informacion a traves de una api de peliculas
+// mientras voy a usar imagenes locales y un array con el nombre de las peliculas
 
-const addNumber = () => {
-  let number;
-  let isNumber = true;
+// la funcionalidad consta de poder elegir una pelicula para ver su informacion.
+// posiblemente pueda agregarle filtros, ya sea por acategoria, anio o lo que sea.
+
+
+const movies = [
+  {
+    title: 'IRON MAN 1',
+    description: 'pelicula de Iron man 1, creada por Marvel.'
+  },
+  {
+    title: 'CAPITAN AMERICA',
+    description: 'pelicula de Capitan America, creada por Marvel.'
+  },
+  {
+    title: 'IRON MAN 2',
+    description: 'pelicula de Iron man 2, creada por Marvel.'
+  },
+  {
+    title: 'HULK',
+    description: 'pelicula de Hulk, creada por Marvel.'
+  },
+  {
+    title: 'THOR',
+    description: 'pelicula de Thor, creada por Marvel.'
+  },
+  {
+    title: 'AVENGERS 1',
+    description: 'pelicula de Avengers 1, creada por Marvel.'
+  }
+]
+
+// funcion para obtener la pelicula que el usuario quiere ver
+// la idea es simular que el usuario escribe en el formulario y presiona el boton de buscar 
+// puede que le agregue un debounce al input (????)
+const chooseYourMovie = () => {
+  let movie;
+
   alert( `
-El programa guarda numeros en un array y los imprime en consola.
-Va a solicitar numeros hasta que se ingrese 0 o un valor no numerico.
-Cuando ocurra lo ultimo va a terminar de pedir e imprimir el array ordenado en cosola.
+El programa cuenta con un arreglo de peliculas (una base de datos).
+En el siguiente prompt tu vas a poder ingresar el nombre de una pelicula.
+Si la misma se encuentra en el arreglo te va a devolver por alert los datos de la misma.
+Si no la encuentra te devolvera "pelicula no encontrada"
+
+peliculas: iron man 1, iron man 2, capitan america, hulk, thor, avengers 1
   `)
-  do {
-    number = parseInt( prompt( 'Ingrese un numero: ' ) )
-    if ( !number ) {
-      isNumber = false
-      console.log( sortNumbers( numbers ) )
-      return
-    }
-    numbers.push( number )
-  } while ( isNumber )
+  movie = prompt( 'Ingrese el nombre de la pelicula a buscar: ' )
+
+  // chequeamos que no haya dejado el prompt vacio
+  if ( movie.length < 1 ) {
+    alert( 'No has ingresado una pelicula' )
+    return
+  }
+
+  // me aseguro que todo el texto este en mayuscula
+  movie = movie.toUpperCase();
+  console.log( movie )
+
+  // busco en el arreglo si coincide el texto ingresado por prompt con el title de alguna de las peliculas
+  const found = movies.find( peli => peli.title == movie )
+  console.log( found )
+
+  // si no existe ya retorno y muestro que no existe
+  if ( !found ) {
+    alert( `La pelicula ${movie} no se encuentra en nuestra base de datos.` )
+    return
+  }
+
+  // si llega hasta aca es porque paso todas las validaciones asi que muestro la descripcion 
+  alert( found.description )
 }
 
-const sortNumbers = ( numbers ) => {
-  const sortedNumbers = numbers.sort( ( a, b ) => a - b )
-  return sortedNumbers
-}
-addNumber()
+chooseYourMovie()
